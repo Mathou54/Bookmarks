22c8d58471f9588f40610d9e625b7ff47ffc0642
Mathou54 <matt54425+github.com@gmail.com>
2018-10-10 11:18:52 +0200

    feat(bookmarks): add reset to filter

--- src/app/bookmarks/bookmarks.component.ts
+++ src/app/bookmarks/bookmarks.component.ts
@@ -9,9 +9,13 @@ import {bookmarks} from './bookmarks.file';
 })
 export class BookmarksComponent implements OnInit {
 
+	public readonly resetSearchKeys: string[] = ['Enter', 'Escape'];
+
 	public search: string = '';
+	public shouldResetSearch: boolean = true;
 
 	public groups: BookmarksGroup[] = bookmarks;
+	public filteredGroups: BookmarksGroup[] = bookmarks;
 
 	constructor() {
 	}
@@ -19,7 +23,22 @@ export class BookmarksComponent implements OnInit {
 	ngOnInit() {
 	}
 
-	filterGroup(groups: BookmarksGroup[], search: string): BookmarksGroup[] {
+	public onKeyUpOnSearch(event: KeyboardEvent): void {
+		if (this.shouldResetSearch) {
+			if (this.resetSearchKeys.includes(event.key)) {
+				this.search = '';
+			}
+		}
+		this.filteredGroups = this.filterGroup(this.groups, this.search);
+	}
+
+	public clickOnBookmark(): void {
+		if (this.shouldResetSearch) {
+			this.search = '';
+		}
+	}
+
+	private filterGroup(groups: BookmarksGroup[], search: string): BookmarksGroup[] {
 		return groups.map(group => {
 			return {
 				title: group.title,
