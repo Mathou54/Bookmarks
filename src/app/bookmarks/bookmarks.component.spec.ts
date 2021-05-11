0be3c9aba9a31d26cf5214aab83416048eff3ef6
Mathou54 <matt54425+github.com@gmail.com>
2021-05-11 10:27:46 +0200

    chore: update dependencies

--- src/app/bookmarks/bookmarks.component.spec.ts
+++ src/app/bookmarks/bookmarks.component.spec.ts
@@ -1,4 +1,4 @@
-import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {ComponentFixture, TestBed, waitForAsync} from '@angular/core/testing';
 
 import {BookmarksComponent} from './bookmarks.component';
 import {FormsModule} from '@angular/forms';
@@ -7,7 +7,7 @@ describe('BookmarksComponent', () => {
 	let component: BookmarksComponent;
 	let fixture: ComponentFixture<BookmarksComponent>;
 
-	beforeEach(async(() => {
+	beforeEach(waitForAsync(() => {
 		TestBed.configureTestingModule({
 			imports: [
 				FormsModule
